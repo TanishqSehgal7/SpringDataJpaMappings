ea38be597eeb8439db0945f5d26190ba890f1008
TanishqSehgal7 <tsgl7246@gmail.com>
2024-10-27 23:56:06 +0530

    Library Management System

---
+++ src/main/java/com/example/springdatajpamappings/springdatajpamappings/entities/library_management_system/Author.java
@@ -0,0 +1,28 @@
+package com.example.springdatajpamappings.springdatajpamappings.entities.library_management_system;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Author")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String authorName;
+
+    @OneToMany(mappedBy = "authorOfTheBooks", fetch = FetchType.EAGER)
+    private List<Book> books;
+
+}
---
+++ src/main/java/com/example/springdatajpamappings/springdatajpamappings/entities/library_management_system/Book.java
@@ -0,0 +1,33 @@
+package com.example.springdatajpamappings.springdatajpamappings.entities.library_management_system;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String bookName;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinTable(name = "book_author_mapping",
+    joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Author authorOfTheBooks;
+
+    private LocalDateTime publishedDate;
+
+}
